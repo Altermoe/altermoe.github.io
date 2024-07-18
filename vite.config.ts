7df26519d7a2a31c099ca48a0794c6dd14e479ab
夕云葛城 <KuroNeko7520@outlook.com>
2024-07-18 16:50:09 +0800

    chore: 初始化项目

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import path from 'node:path'
+import { defineConfig } from 'vite'
+
+export default defineConfig(() => {
+  return {
+    define: {
+      // 关闭选项式 API 支持
+      __VUE_OPTIONS_API__: false,
+
+      // 在生产环境中关闭 devtools 支持
+      __VUE_PROD_DEVTOOLS__: false,
+
+      // 禁用生产版本中水合不匹配的详细警告以优化
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+    },
+
+    server: {
+      host: '0.0.0.0',
+      port: 12200,
+      cors: true,
+    },
+
+    preview: {
+      host: '0.0.0.0',
+      port: 12201,
+      cors: true,
+    },
+
+    resolve: {
+      alias: [
+        {
+          find: /^@\//,
+          replacement: `${path.resolve(__dirname, 'src')}/`,
+        },
+      ],
+    },
+
+    plugins: [],
+  }
+})
