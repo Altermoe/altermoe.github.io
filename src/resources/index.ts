9300ddbfa2ee80eb0011477e80951c5b10620da5
夕云葛城 <KuroNeko7520@outlook.com>
2024-08-01 02:56:10 +0800

    fix(resource): 因 hash 导致索引资源失败的问题

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -13,7 +13,7 @@ export const resourceManager: Plugin = {
       const family = url.split('/').at(-1)
       if (!family)
         return
-      const fontface = new FontFace(family.replace(/\..+/, ''), `url(${url})`)
+      const fontface = new FontFace(family.replace(/(\-[A-Za-z0-9]+)?\..+/, ''), `url(${url})`)
       await fontface.load()
       if (document.fonts)
       document.fonts.add(fontface)
