7e3f5786cf98b7a5af4760fc67f4865decbe386e
夕云葛城 <KuroNeko7520@outlook.com>
2024-08-01 02:35:42 +0800

    feat(components): add win-button

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'WinButton',
+})
+
+defineProps<{
+  disabled?: boolean
+  square?: boolean
+  circle?: boolean
+}>()
+</script>
+
+<template>
+  <button
+    class="win-button"
+    :class="{
+      'is-square': square,
+      'is-circle': circle,
+    }"
+  >
+    <slot name="default" />
+  </button>
+</template>
+
+<style lang="scss">
+.win-button {
+  @apply
+    py-[5px] px-[12px] rounded
+    border border-stroke-circleElevationBorder-0 border-b-stroke-circleElevationBorder-100
+    bg-fill-controlDefault
+    text-xs leading-[20px] text-text-primary font-[SegoeIcons]
+    transition
+    ease-out
+  ;
+
+  &:hover {
+    @apply bg-fill-controlSecondary;
+  }
+
+  &:active {
+    @apply
+      border-b-stroke-circleElevationBorder-0
+      text-text-secondary
+      bg-fill-controlTertiary
+    ;
+  }
+
+  &.is-square, &.is-circle {
+    @apply w-8 h-8 px-[5px];
+  }
+
+  &.is-circle {
+    @apply
+      rounded-full
+      border-b-stroke-circleElevationBorder-0
+    ;
+  }
+}
+</style>
---
+++ src/components/Button/index.ts
@@ -0,0 +1,1 @@
+export { default as WinButton } from './Button.vue'
