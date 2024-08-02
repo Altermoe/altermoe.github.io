7c9c60ce87e68c701755d6f9943e8ffce4db5e4d
夕云葛城 <KuroNeko7520@outlook.com>
2024-08-03 03:56:13 +0800

    feat(Development): 完善开发者页面

--- src/pages/development/Development.vue
+++ src/pages/development/Development.vue
@@ -1,3 +1,72 @@
+<script setup lang="ts">
+import { WinText } from '@/components'
+import { SKILL_LIST } from './data'
+</script>
+
 <template>
-  开发者
+  <div class="p-6 h-full flex flex-col overflow-auto min-w-[408px]">
+    <div v-for="group in SKILL_LIST" :key="group.title" class="mb-10">
+      <WinText tag="div" type="subtitle" class="mb-3">
+        {{ group.title }}
+      </WinText>
+      <div class="flex flex-wrap gap-4">
+        <a
+          v-for="item in group.children"
+          :key="item.title"
+          :href="item.link"
+          class="card"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <component :is="item.icon" class="w-12 h-12" />
+          <div class="content">
+            <WinText type="body-strong" inline class="flex-shrink-0">
+              {{ item.title }}
+            </WinText>
+            <WinText type="caption" class="">
+              {{ item.summary }}
+            </WinText>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.card {
+  @apply
+    box-border overflow-hidden
+    border border-stroke-controlDefault
+    w-[360px] h-[90px]
+    px-5 rounded
+    flex items-center gap-4
+    bg-fill-controlDefault
+    select-none
+    cursor-pointer
+  ;
+
+  &:hover {
+    @apply
+      border-stroke-controlSecondary
+      bg-fill-controlSecondary
+    ;
+  }
+
+  &:active {
+    @apply
+      border-stroke-controlElevationBorder
+      bg-fill-controlTertiary
+    ;
+  }
+}
+
+.content {
+  @apply
+    flex-1
+    max-h-full overflow-hidden
+    flex flex-col gap-0.5
+    text-xs
+  ;
+}
+</style>
---
+++ src/pages/development/data/skills.ts
@@ -0,0 +1,133 @@
+import {
+  DeckGL,
+  Electron,
+  Node,
+  Python,
+  React,
+  RxJS,
+  Tailwindcss,
+  TypeScript,
+  Vite,
+  Vue,
+  Windows,
+} from '@/icons'
+
+interface Skill {
+  title: string
+  children: {
+    title: string
+    summary: string
+    link: string
+    icon: Component
+  }[]
+}
+
+export const SKILL_LIST: Skill[] = [
+  {
+    title: '编程语言（含运行时）',
+    children: [
+      {
+        title: 'TypeScript',
+        summary: 'TypeScript is a strongly typed programming language that builds on JavaScript, giving you better tooling at any scale.',
+        link: 'https://www.typescriptlang.org/',
+        icon: TypeScript,
+      },
+      {
+        title: 'Node.js',
+        summary: 'Node.js® 是一个免费、开源、跨平台的 JavaScript 运行时环境，它让开发人员能够创建服务器、Web 应用、命令行工具和脚本。',
+        link: 'https://nodejs.org/',
+        icon: Node,
+      },
+      {
+        title: 'Python',
+        summary: 'Python is a programming language that lets you work quickly and integrate systems more effectively.',
+        link: 'https://www.python.org/',
+        icon: Python,
+      },
+    ]
+  },
+
+  {
+    title: 'Web 开发框架',
+    children: [
+      {
+        title: 'Vue.js',
+        summary: '渐进式 JavaScript 框架。',
+        link: 'https://cn.vuejs.org/',
+        icon: Vue,
+      },
+      {
+        title: 'React.js',
+        summary: 'The library for web and native user interfaces.',
+        link: 'https://react.dev/',
+        icon: React,
+      },
+    ],
+  },
+
+  {
+    title: '视觉开发',
+    children: [
+      {
+        title: 'tailwindcss',
+        summary: '只需书写 HTML 代码，无需书写 CSS，即可快速构建美观的网站。',
+        link: 'https://tailwindcss.com/',
+        icon: Tailwindcss,
+      },
+      {
+        title: 'Fluent Design 2',
+        summary: 'Fluent 2 provides a seamless maker experience from design to development to delivery.',
+        link: 'https://fluent2.microsoft.design/',
+        icon: Windows,
+      }
+    ],
+  },
+
+  {
+    title: 'WebGL 开发',
+    children: [
+      {
+        title: 'Deck.GL',
+        summary: 'GPU-powered, highly performant large-scale data visualization.',
+        link: 'https://deck.gl/',
+        icon: DeckGL,
+      },
+    ],
+  },
+
+  {
+    title: '本地开发',
+    children: [
+      {
+        title: 'Electron',
+        summary: 'Build cross-platform desktop apps with JavaScript, HTML, and CSS.',
+        link: 'https://www.electronjs.org/',
+        icon: Electron,
+      },
+    ],
+  },
+
+  {
+    title: '工程化',
+    children: [
+      {
+        title: 'Vite.js',
+        summary: '下一代的前端工具链。为开发提供极速响应。',
+        link: 'https://cn.vitejs.dev/',
+        icon: Vite,
+      },
+    ],
+  },
+
+  {
+    title: '编程范式',
+    children: [
+      {
+        title: 'RxJS',
+        summary: 'Reactive Extensions Library for JavaScript',
+        link: 'https://rxjs.dev/',
+        icon: RxJS,
+      },
+    ],
+  },
+]
