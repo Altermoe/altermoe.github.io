68991402586d97a1077bf39ce01b050ca3a8f1c1
夕云葛城 <KuroNeko7520@outlook.com>
2024-08-06 22:28:10 +0800

    chore(package): add monaco-editor

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -1,7 +1,23 @@
 import type { Plugin } from 'vue'
+import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+import JSONWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
+import TSWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+import HTMLWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
 
 export const resourceManager: Plugin = {
   install: async () => {
+    // setup monaco-editor envs
+    Reflect.set(globalThis, 'MonacoEnvironment', {
+      getWorker: async (_: unknown, label: string) => {
+        return new ({
+          html: HTMLWorker,
+          json: JSONWorker,
+          javascript: TSWorker,
+          typescript: TSWorker,
+        }[label] ?? EditorWorker)
+      }
+    })
+
     // install fonts
     const fontURLMap = import.meta.glob<boolean, string, string>('./fonts/*.ttf', {
       query: '?url',
