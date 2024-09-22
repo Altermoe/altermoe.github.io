cf0b782f776b4b28279cfc434c9904805a3fe6e0
夕云葛城 <KuroNeko7520@outlook.com>
2024-09-22 14:22:10 +0800

    fix(HeaderBar): 修改折叠菜单的图标

--- src/layouts/components/HeaderBar.vue
+++ src/layouts/components/HeaderBar.vue
@@ -16,7 +16,12 @@ const collapse = defineModel<boolean>('collapse', {
     "
   >
     <WinButton square @click="collapse = !collapse">
-      {{ collapse ? '' : '' }}
+      <svg  viewBox="0 0 1024 1024" version="1.1" fill="currentColor">
+        <path d="M192 192m-64 0a64 64 0 1 0 128 0 64 64 0 1 0-128 0Z"></path>
+        <path d="M192 512m-64 0a64 64 0 1 0 128 0 64 64 0 1 0-128 0Z" p-id="4267"></path>
+        <path d="M192 832m-64 0a64 64 0 1 0 128 0 64 64 0 1 0-128 0Z" p-id="4268"></path>
+        <path d="M864 160H352c-17.7 0-32 14.3-32 32s14.3 32 32 32h512c17.7 0 32-14.3 32-32s-14.3-32-32-32zM864 480H352c-17.7 0-32 14.3-32 32s14.3 32 32 32h512c17.7 0 32-14.3 32-32s-14.3-32-32-32zM864 800H352c-17.7 0-32 14.3-32 32s14.3 32 32 32h512c17.7 0 32-14.3 32-32s-14.3-32-32-32z"></path>
+      </svg>
     </WinButton>
 
     <WinButton circle @click="isDark = !isDark">
