d21d96c9a3c10e4a22ab35c060d890be6eb14d9b
夕云葛城 <KuroNeko7520@outlook.com>
2024-10-24 23:23:27 +0800

    feat: 开发工具组件

---
+++ src/pages/tools/Tool.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="p-6 h-full overflow-auto gap-4 flex flex-wrap place-content-start">
+    <div
+      v-for="i in 20"
+      :ley="i"
+      class="tool-item"
+      :class="{
+        large: i % 5 === 0
+      }"
+    >
+      {{ i }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.tool-item {
+  @apply
+    w-24 h-24 rounded
+    border border-color-NeutralStroke1
+    bg-color-NeutralBackground1
+    select-none
+    cursor-pointer
+  ;
+
+  &:hover {
+    @apply
+      border-color-NeutralStroke1Hover
+      bg-color-NeutralBackground1Hover
+    ;
+  }
+
+  &:active {
+    @apply
+      border-color-NeutralStroke1Pressed
+      bg-color-NeutralBackground1Pressed
+    ;
+  }
+}
+</style>
