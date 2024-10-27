dc6b1afb5d5e5a60634cbb32cdb896d6fcbfb62d
夕云葛城 <KuroNeko7520@outlook.com>
2024-10-27 20:50:26 +0800

    chore(main.ts): inject debug tool

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { LayoutIndex } from '@/layouts'
-
 </script>
 
 <template>
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,18 @@ import App from './App.vue'
 import './styles/index.css'
 import { router } from '@/router'
 import { resourceManager } from './resources'
+import { createPinia } from 'pinia'
+import { BSON } from 'bson'
 
 const app = createApp(App)
 
 app
+  .use(createPinia())
   .use(router)
   .use(resourceManager)
   .mount('#app')
+
+Reflect.set(globalThis, 'BSON' , {
+  serialize: BSON.serialize,
+  deserialize: BSON.deserialize,
+})
