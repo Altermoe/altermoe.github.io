6858767be49aefb23f5ae37becada257916b7335
夕云葛城 <KuroNeko7520@outlook.com>
2024-10-28 20:51:18 +0800

    feat(stores/socket.ts): 增加用于管理 websocket 的 store

---
+++ src/stores/socket.ts
@@ -0,0 +1,41 @@
+import { defineStore } from 'pinia'
+import { io } from 'socket.io-client'
+import type { Socket } from 'socket.io-client'
+import { BSON } from 'bson'
+
+declare namespace WsData {
+  interface ClientUpdate {
+    clients: string[]
+  }
+
+  interface ClientInfo {
+    id: string
+  }
+}
+
+export const useSocketStore = defineStore('socket', () => {
+  const socket = io('ws://192.168.123.48:3000')
+
+  const status = ref('disconnect')
+
+  socket.on('connect', () => {
+    status.value = 'connect'
+  })
+
+  socket.on('disconnect', () => {
+    status.value = 'disconnect'
+  })
+
+  const clients = ref<string[]>([])
+
+  socket.on('clientsUpdate', async(message: Uint8Array) => {
+    const data = (BSON.deserialize(message) as WsData.ClientUpdate)
+    clients.value = data.clients
+  })
+
+  return {
+    socket: socket as Socket,
+    status,
+    clients,
+  }
+})
